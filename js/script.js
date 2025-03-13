7413a33ff8a8d846b708b186dc4d2608e454cc11
charlesdottir <charles@dottirlabs.com>
2025-03-13 09:06:59 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -513,7 +513,8 @@
      		 */
 
 		
-		  window.scroll(function() {
+		$(window).on("load",function() {
+		  $(window).scroll(function() {
 		    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
 		    $(".fade").each(function() {
 		      /* Check the location of each desired element */
@@ -521,12 +522,13 @@
 		      
 		      /* If the element is completely within bounds of the window, fade it in */
 		      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-			if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+		        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
 		      } else { //object goes out of view (scrolling up)
-			if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+		        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
 		      }
 		    });
 		  }).scroll(); //invoke scroll-handler on page-load
+		});
 		
 
 		/**
