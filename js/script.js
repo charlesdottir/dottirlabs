b8a25b5f9a0e6c193322c86785410dbedcebc336
charlesdottir <charles@dottirlabs.com>
2025-03-13 21:57:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -573,7 +573,7 @@
 			    var objectWidth=$(this).outerWidth();
 		
 			    if (objectTop < scrollTop) {
-			      $(this).css({left: (100-(100*(scrollTop-objectBottom)/objectHeight))+"%"}); 
+			      $(this).css({left: (100-(100*(objectBottom-scrollTop)/objectHeight))+"%"}); 
 			      $(this).css({opacity: (100*(objectBottom-scrollTop)/objectHeight)+"%"}); 
 			    }
 
