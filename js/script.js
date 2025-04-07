acb907e8bbb381bd327e757d96d4c670e27b7fc5
charlesdottir <charles@dottirlabs.com>
2025-04-07 10:36:35 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -451,13 +451,13 @@
 			swiper.decor.el = document.createElement( 'div' );
 			swiper.decor.el.classList.add( 'swiper-decorative' );
 			swiper.decor.circle = calcCircle( swiper );
-			swiper.decor.el.innerHTML = '<div class="swiper-decorative-circle" style="left:'+swiper.decor.circle.centerX+100+'px;top:'+swiper.decor.circle.centerY+'px;width:'+(swiper.decor.circle.radius*2)+'px;height:'+(swiper.decor.circle.radius*2)+'px"></div>';
+			swiper.decor.el.innerHTML = '<div class="swiper-decorative-circle" style="left:'+swiper.decor.circle.centerX+100+'px;top:'+swiper.decor.circle.centerY+'px;width:'+(swiper.decor.circle.radius*2-300)+'px;height:'+(swiper.decor.circle.radius*2-300)+'px"></div>';
 			swiper.$el[0].insertBefore( swiper.decor.el, swiper.$wrapperEl[0] );
 			swiper.decor.circleEl = swiper.decor.el.querySelector('.swiper-decorative-circle');
 
 			swiper.on( 'resize', function () {
 				swiper.decor.circle = calcCircle( swiper );
-				swiper.decor.circleEl.setAttribute( 'style', 'left:'+swiper.decor.circle.centerX+100+'px;top:'+swiper.decor.circle.centerY+'px;width:'+(swiper.decor.circle.radius*2)+'px;height:'+(swiper.decor.circle.radius*2)+'px' );
+				swiper.decor.circleEl.setAttribute( 'style', 'left:'+swiper.decor.circle.centerX+100+'px;top:'+swiper.decor.circle.centerY+'px;width:'+(swiper.decor.circle.radius*2-300)+'px;height:'+(swiper.decor.circle.radius*2-300)+'px' );
 			});
 
 			swiper.on( 'slideChangeTransitionStart', function () {
